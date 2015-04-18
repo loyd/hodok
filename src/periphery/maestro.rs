c1385683357058fe340343abd84b3cf928c0d598
Paul Loyd <pavelko95@gmail.com>
2015-04-18 22:01:46 +0300

    feat: mini maestro driver

---
+++ src/periphery/maestro.rs
@@ -0,0 +1,18 @@
+use std::io::Result;
+
+use ifaces::Serial;
+
+
+pub struct Maestro(Serial);
+
+impl Maestro {
+    pub fn new(device: &str) -> Result<Maestro> {
+        Ok(Maestro(try!(Serial::open(device))))
+    }
+
+    pub fn set_target(&self, channel: u8, mut us: u16) -> Result<()> {
+        us <<= 2;
+        let (lb, mb) = ((us & 0x7f) as u8, ((us >> 7) & 0x7f) as u8);
+        Ok(try!(self.0.write(&[0x84, channel, lb, mb])))
+    }
+}
